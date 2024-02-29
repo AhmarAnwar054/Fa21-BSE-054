d0c148ace6b102b58fd64f51ceec4ce2b4196fd4
AhmarAnwar054 <ahmaranwar6505@gmail.com>
2024-02-29 20:16:24 +0500

    Assignment 1 Complete

--- ClassAssignment/Assignment#1/ArraysFunction.js
+++ ClassAssignment/Assignment#1/ArraysFunction.js
@@ -1,4 +1,4 @@
-const myFavColors = ["Orange", "Blue", "Green", "Red"]
+const myFavColors = ["Orange", "Blue", "Green", "Red"];
 console.log(myFavColors.length);
 console.log(myFavColors.toString());
 console.log(myFavColors.at(2));
@@ -14,37 +14,59 @@ console.log(myFavColors.shift());
 console.log(myFavColors);
 console.log(myFavColors.unshift("Purple"));
 console.log(myFavColors);
-myFavColors[0] = "LightBlue"
+myFavColors[0] = "LightBlue";
 console.log(myFavColors);
-myFavColors[myFavColors.length] = "Cyan"
+myFavColors[myFavColors.length] = "Cyan";
 console.log(myFavColors);
-delete myFavColors[2]
+delete myFavColors[2];
 console.log(myFavColors);
-const myFavSports = ["Cricket", "Tennis", "FootBall"]
-const myFavSubjects = ["Maths", "Programming", "Business"]
+const myFavSports = ["Cricket", "Tennis", "FootBall"];
+const myFavSubjects = ["Maths", "Programming", "Business"];
 console.log(myFavColors.concat(myFavSports, myFavSubjects));
 console.log(myFavColors.concat("myFavSports", myFavSubjects));
 console.log(myFavSubjects);
-myFavSubjects.copyWithin(2, 0) //Copy to index 2, all elements from index 0:
+myFavSubjects.copyWithin(2, 0); //Copy to index 2, all elements from index 0:
 console.log(myFavColors);
-console.log(myFavColors.copyWithin(2, 0, 2));//Copy to index 2, the elements from index 0 to 2(excluded):
+console.log(myFavColors.copyWithin(2, 0, 2)); //Copy to index 2, the elements from index 0 to 2(excluded):
 console.log(myFavColors.copyWithin(-2, 0, 2));
-const myFavNumbers = [3, 7, 9, 11, 13]
+const myFavNumbers = [3, 7, 9, 11, 13];
 console.log(myFavNumbers.copyWithin(1, -3, -1)); //2,3
-const nestedArray = [[1, 2], [3, 4], [5, 6, [7, 8]]] //--->[1,2,3,4,5,6,7,8]
+const nestedArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8]],
+]; //--->[1,2,3,4,5,6,7,8]
 console.log(nestedArray[2][2][1]);
 console.log(nestedArray);
-console.log(nestedArray.flat(2));//flat brings all the values at (specified depth)n-depth to depth 0
+console.log(nestedArray.flat(2)); //flat brings all the values at (specified depth)n-depth to depth 0
 console.log(myFavSubjects);
-myFavSubjects.splice(0, 1, "Data Science", "Stats")
+myFavSubjects.splice(0, 1, "Data Science", "Stats");
 console.log(myFavSubjects);
 console.log(myFavSports);
-newArrayAfterSplicing = myFavSports.toSpliced(0, 1, "Hockey")
+newArrayAfterSplicing = myFavSports.toSpliced(0, 1, "Hockey");
 console.log(myFavSports);
 console.log(newArrayAfterSplicing);
-const difficultSubjects = myFavSubjects.slice(0, 3)
+const difficultSubjects = myFavSubjects.slice(0, 3);
 console.log(difficultSubjects);
 console.log("This is a Comma Seprated String " + difficultSubjects);
+//map()
+let func1 = function (x, y) {
+  return x * y; //current item * its index value
+};
+let returnedArray = myFavNumbers.map(func1);
+console.log(returnedArray);
 
+//filter()
+let func2 = function (x) {
+  return x % 3 == 0;
+};
+returnedArray = myFavNumbers.filter(func2);
+console.log(returnedArray);
 
-
+//reduce()
+let func3 = function (x, y) {
+  return x + y;
+};
+console.log("myFavNumbers:", myFavNumbers); //[3, 9, 11, 11, 13]
+let sum = myFavNumbers.reduce(func3, 100);
+console.log(sum);
--- ClassAssignment/Assignment#1/StringFunctions.js
+++ ClassAssignment/Assignment#1/StringFunctions.js
@@ -1,36 +1,35 @@
 //length property
-let myName  = "Ahmar Anwar"
-console.log(myName.length)  //returns the length of a string or number of characters..
+let myName = "Ahmar Anwar";
+console.log(myName.length); //returns the length of a string or number of characters..
 
 //Extracting Characters from String
 //4 methods
 console.log(myName.at(7));
 console.log(myName.charAt(7));
 console.log(myName[7]);
-console.log(myName.charAt(myName.length-4))
-console.log(myName.at(-4))
+console.log(myName.charAt(myName.length - 4));
+console.log(myName.at(-4));
 
-myName[0] = "a"
+myName[0] = "a";
 
 console.log(myName); //Gives no error, but does not work (as string is immutable)
-
 
 //Extracting String Parts
 //3 methods
 console.log(myName.slice(-5));
-console.log(myName.substring(0,5))
-console.log(myName.substr(-5))
+console.log(myName.substring(0, 5));
+console.log(myName.substr(-5));
 
 //Converting to Upper and Lower Case
-console.log(myName.toLowerCase())
+console.log(myName.toLowerCase());
 console.log(myName.toUpperCase());
 
 //Joining two or more strings together
 //2 methods
-console.log("Muhammad".concat(" ",myName))
-console.log("Muhammad"+" "+myName);
+console.log("Muhammad".concat(" ", myName));
+console.log("Muhammad" + " " + myName);
 //removes whitespaces from  a string:
-myString = "   SomeString   "
+myString = "   SomeString   ";
 console.log(myString);
 console.log(myString.trim());
 console.log(myString.trimStart());
@@ -38,27 +37,30 @@ console.log(myString.trimEnd());
 
 //String Padding
 //2 methods
-let myNumberInString = "5"
-console.log(myNumberInString.padStart(4,"0"));    
-console.log(myNumberInString.padEnd(4,"0"));  
-let myNumber = 5
-console.log(myNumber.toString().padEnd(2,"$"));
+let myNumberInString = "5";
+console.log(myNumberInString.padStart(4, "0"));
+console.log(myNumberInString.padEnd(4, "0"));
+let myNumber = 5;
+console.log(myNumber.toString().padEnd(2, "$"));
 
 //String Repeat
 console.log(myNumberInString.repeat(5));
 
 //Replacing String Content
-console.log(myName.replace("Ahmar","Umar"));
-     
+console.log(myName.replace("Ahmar", "Umar"));
+
 //Regular Expression with the flag /g(global match)
-let anotherString = "Hello Sir Ali and Sir Salman"
-console.log(anotherString.replace(/Sir/g,"Mr."));
-
-
+let anotherString = "Hello Sir Ali and Sir Salman";
+console.log(anotherString.replace(/Sir/g, "Mr."));
+//or
+console.log(anotherString.replaceAll("Sir", "Mr."));
+console.log(anotherString.replaceAll(/Sir/g, "Mr."));
 //Converting a String to an Array
-let searchString = "A brown bag with purple color flower on it."
-let wordsFromSearchString = searchString.split(" ",2) //Split on spaces
+let searchString = "A brown bag with purple color flower on it.";
+let wordsFromSearchString = searchString.split(" "); //Split on spaces
 console.log(wordsFromSearchString);
 
-
 //indexOf
+
+console.log(searchString.indexOf("brown"));
+console.log(searchString.indexOf("b", 3));
